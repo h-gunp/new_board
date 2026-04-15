930bdd3441c104d05f3d0bf9ab9abce0169926b3
ghdrjsvy0826 <hjshgp13@gmail.com>
2026-04-16 01:57:58 +0900

    v.1

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, url_for, redirect
+import sqlite3
+
+def db_connection(): 
+  conn = sqlite3.connect("board.db")
+  conn.row_factory = sqlite3.Row
+  print("db 연결 완료")
+
+  return conn
+
+def init_db():
+  conn = db_connection()
+  cursor = conn.cursor()
+
+  cursor.execute("""CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id NOT NULL UNIQUE, user_ps NOT NULL)""")
+  cursor.execute("""CREATE TABLE IF NOT EXISTS posts(id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(100) NOT NULL, body TEXT NOT NULL)""")
+  cursor.execute("""INSERT OR IGNORE INTO users(user_id, user_ps) VALUES ('admin', '1234')""")
+  
+  conn.commit()
+  conn.close()
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    user_id = request.form['user_id']
+    user_ps = request.form['user_ps']
+    
+    conn = db_connection()
+    try:
+      cursor = conn.cursor()
+      sql = "SELECT * FROM users WHERE user_id = ? AND user_ps = ?"
+      cursor.execute(sql, (user_id, user_ps))
+      user = cursor.fetchone()
+
+      if user:
+        return redirect(url_for('main'))
+      else:
+        return redirect(url_for('login'))
+      
+    finally:
+      conn.close()
+
+  return render_template('login.html')
+
+@app.route('/register')
+def register():
+  return render_template('register.html')
+
+@app.route('/main')
+def main():
+  return render_template('main.html')
+
+
+if __name__ == '__main__':
+    init_db()
+    app.run(port=8081, debug=True)
