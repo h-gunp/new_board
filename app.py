63a1a0e0d1425a1bbfbc10fd98a4083213859010
홍건표 <hjshgp13@gmail.com>
2026-04-16 14:13:27 +0900

    ver.2

--- app.py
+++ app.py
@@ -44,14 +44,117 @@ def login():
 
   return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods = ['GET', 'POST'])
 def register():
+  if request.method == 'POST':
+    user_id = request.form['user_id']
+    user_ps = request.form['user_ps']
+
+    conn = db_connection()
+    try:
+      cursor = conn.cursor()
+      sql = "INSERT INTO users (user_id, user_ps) VALUES (?, ?)"
+      cursor.execute(sql, (user_id, user_ps))
+      conn.commit()
+
+      return redirect(url_for('login'))
+    except sqlite3.IntegrityError:
+      return "이미 존재하는 아이디입니다. <a href="/">메인페이지로 돌아가기</a>"
+    finally:
+      conn.close()
+
   return render_template('register.html')
 
 @app.route('/main')
 def main():
-  return render_template('main.html')
+  conn = db_connection()
+  try:
+    cursor = conn.cursor()
 
+    sql = "SELECT * FROM posts ORDER BY id DESC"
+    cursor.execute(sql)
+
+    posts = cursor.fetchall()
+
+  finally:
+    conn.close()
+
+  return render_template('main.html', posts=posts)
+
+@app.route('/create', methods = ['GET', 'POST'])
+def create():
+  if request.method == 'POST':
+    title = request.form['title']
+    body = request.form['body']
+    conn = db_connection()
+    try:
+      cursor = conn.cursor()
+      sql = "INSERT INTO posts (title, body) VALUES (?, ?)"
+      cursor.execute(sql, (title, body))
+      conn.commit()
+      return redirect(url_for('main'))
+    
+    finally:
+      conn.close()
+
+  return render_template('create.html')
+
+@app.route('/read/<int:id>')
+def read(id):
+  conn = db_connection()
+  try:
+    cursor = conn.cursor()
+    sql = "SELECT * FROM posts WHERE id = ?"
+    cursor.execute(sql, (id, ))
+    post = cursor.fetchone()
+    if not post:
+      return "존재하지 않는 게시글입니다. <a href="/">메인페이지로 돌아가기</a>"
+
+  finally:
+    conn.close()
+
+  return render_template('read.html', post=post)
+
+@app.route('/update/<int:id>', methods = ['GET', 'POST'])
+def update(id):
+  if request.method == 'POST':
+    title = request.form['title']
+    body = request.form['body']
+    conn = db_connection()
+    try:
+      cursor = conn.cursor()
+      sql = "UPDATE posts SET title = ?, body = ? WHERE id = ?"
+      cursor.execute(sql, (title, body, id))
+      conn.commit()
+      return redirect(url_for('main'))
+    
+    finally:
+      conn.close()
+  else:
+    conn = db_connection()
+    try:
+      cursor = conn.cursor()
+      sql = "SELECT * FROM posts WHERE id = ?"
+      cursor.execute(sql, (id, ))
+      post = cursor.fetchone()
+      if not post:
+        return '존재하지 않는 게시글입니다. <a href="/">메인페이지로 돌아가기</a>'
+    finally:
+      conn.close()
+
+  return render_template('update.html', post=post)
+
+@app.route('/delete/<int:id>', methods = ['GET', 'POST'])
+def delete(id):
+  conn = db_connection()
+  try:
+    cursor = conn.cursor()
+    sql = "DELETE FROM posts WHERE id = ?"
+    cursor.execute(sql, (id, ))
+    conn.commit()
+  finally:
+    conn.close()
+  return '삭제되었습니다. <a href="/main">메인페이지로 돌아가기</a>'
 
 if __name__ == '__main__':
     init_db()
